6fc7b165d0bd895390078ae8757c352047acb2bc
Prakash Kumar <send2prakashkumar@gmail.com>
2020-01-14 22:26:48 +0530

    Add files via upload

--- W3/7_2.constant array.php
+++ W3/7_2.constant array.php
@@ -1,9 +1,12 @@
 <?php
 #creating constant arrays
-define("array",["a","b"]);
-echo array[0];
+define("arry",["a","b"]);
+echo arry[0];
+
 
 //output : PHP Parse error:  syntax error, unexpected '[', expecting '(' in /home/prakash/Study/PHP/W3/Constants/7_2.constant array.php on line 4
+//changed the name array -> arry
+//array is predefined keyword
 ?>
 
 
