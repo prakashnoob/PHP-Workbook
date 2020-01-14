89a810c90a879db9cf0b284aa781e8162a05b983
Prakash Kumar <send2prakashkumar@gmail.com>
2020-01-14 23:58:39 +0530

    Add files via upload

---
+++ W3/7_4.Union operation.php
@@ -0,0 +1,25 @@
+<?php
+#union operation +
+
+$a= array("a"=>"check","b"=>"nothing");
+
+$b=array("d"=>"check","e"=>"everything");
+
+print_r($a+$b);
+
+
+
+/*Array
+(
+    [a] => check
+    [b] => nothing
+    [d] => check
+    [e] => everything
+)
+
+*/
+//
+//
+?>
+
+
