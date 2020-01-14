502994962a420f337381ab7edce1271ba20c58b6
Prakash Kumar <send2prakashkumar@gmail.com>
2020-01-14 22:25:32 +0530

    Add files via upload

---
+++ W3/7_2.constant array.php
@@ -0,0 +1,9 @@
+<?php
+#creating constant arrays
+define("array",["a","b"]);
+echo array[0];
+
+//output : PHP Parse error:  syntax error, unexpected '[', expecting '(' in /home/prakash/Study/PHP/W3/Constants/7_2.constant array.php on line 4
+?>
+
+
